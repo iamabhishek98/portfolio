2f74e627b28c8dee4cacb7335d31bbd7bd2048fc
Abhishek <saiabhishek.karnati@u.nus.edu>
2021-08-08 17:13:57 +0800

    updated experiences

--- src/containers/Experiences/Experiences.js
+++ src/containers/Experiences/Experiences.js
@@ -3,66 +3,108 @@ import Experience from "./ExperienceTemplate";
 
 class ExperiencesContainer extends Component {
   render() {
-    const Hp = {
-      Role: "software developer intern",
+    const garuda = {
+      Role: "software engineer intern",
+      Name: "garuda robotics",
+      Duration: "may 2021 – aug 2021",
+      Description: [
+        `Led full-stack development of an Unmanned Aircraft Traffic Management (UTM) system 
+        with Unmanned Aerial Vehicle (UAV) surveillance capabilities from initial stages to 
+        a stage ready for deployment to clients`,
+        `Designed scalable backend microservices to detect drone violations and created alert 
+        propagation pipelines while integrating in-house and third-party services to create 
+        an operations map for real-time drone monitoring`,
+        `Carried out detailed code reviews and conducted company-wide workshops on writing clean, 
+        performant and high-quality code leading to appreciation from CTO`,
+        `Tech Stack: JavaScript, React.js, Leaflet, Node.js, RESTful & WebSocket APIs, RabbitMQ, 
+        MongoDB, Mongoose, Mocha`,
+      ],
+    };
+
+    const sensorflow = {
+      Role: "software engineer intern",
+      Name: "sensorflow",
+      Duration: "jul 2020 - dec 2020",
+      Description: [
+        `Led full-stack development of a secure application allowing management of clients 
+        and deployed IOT sensors`,
+        `Designed GraphQL queries/mutations to efficiently propagate data between server and 
+        client while also automating unit, integration and e2e testing to achieve code coverage > 90%`,
+        `Spearheaded SQL database schema discussions and designed complex database queries 
+        to optimize inefficient backend code (order of several seconds)`,
+        `Conducted internal product discussions to ensure smooth flow of user experience 
+        requirements from product managers to developers and planned agile SCRUM sprints`,
+        `Tech Stack: TypeScript, Nest.js, RESTful APIs, Jest, GraphQL, PostgreSQL, TypeORM, Docker`,
+      ],
+    };
+
+    const hp = {
+      Role: "software engineer intern",
       Name: "hewlett-packard",
       Duration: "may 2020 – jul 2020",
-      Description: [],
+      Description: [
+        `Identified and analysed opportunities for process improvement and took initiative 
+        to develop a new large-scale full-stack application to support data automation in 
+        internal processes on a global scale`,
+        `Collaborated with functional teams to create efficient algorithms to draw intelligence 
+        from raw existing data and was awarded a Certificate of Recognition for outstanding contributions `,
+        `Tech Stack: JavaScript, React.js, Node.js, PostgreSQL, VB, UiPath, Power BI, Power Automate`,
+      ],
     };
 
-    const Nus_SA = {
-      Role: "cp2106 senior advisor",
-      Name: "nus",
-      Duration: "may 2020 – aug 2020",
-      Description: [],
+    const orbital_TA = {
+      Role: "senior advisor for cp2106",
+      Name: "nus computing",
+      Duration: "may 2020 – aug 2021",
+      Description: [
+        `Supervised 30 teams in completion of advanced software projects and guided them 
+        by providing architectural advice along with troubleshooting their issues`,
+        `Evaluated monthly progress by ensuring they adhered to following good software 
+        engineering practices and giving constructive feedback`,
+        `Received overwhelmingly positive feedback due to many teams being able to achieve 
+        targets exceeding initial expectations`,
+      ],
     };
 
-    const Amaris = {
+    const amaris = {
       Role: "computer vision intern",
       Name: "amaris.ai",
       Duration: "dec 2019 – jan 2020",
       Description: [
-        `Developed solutions for object detection and user action recognition applications`,
-        `Trained Deep Learning models to detect offenders like PMD riders and motorcyclists 
-        without safety headgear`,
-        `Initiated and Implemented novel algorithms from scratch to perform exercise counting`,
+        `Leveraged on cutting-edge computer vision algorithms to automate image processing 
+        tasks like data augmentation and object detection`,
+        `Implemented algorithms to innovatively solve action recognition problems using 
+        pose estimation which earned appreciation from company’s Chief Innovation Officer`,
+        `Tech Stack: OpenCV, NumPy/Pandas, Detectron2, YOLOv3`,
       ],
     };
 
-    const Nus_TA = {
-      Role: "undergraduate teaching assistant",
-      Name: "nus",
-      Duration: "aug 2019 – nov 2019",
+    const cg1111_TA = {
+      Role: "teaching assistant for cg1111",
+      Name: "nus computing",
+      Duration: "aug 2019 – dec 2019",
       Description: [
-        `Nominated as a Teaching Assistant for CG1111 upon
+        `Selected to be a Teaching Assistant for CG1111 (Engineering Principles & Practices) upon
         performing outstandingly well in the module`,
-        `Explained concepts, clarified doubts as well as
-        assisted a group of 10 students with debugging their Arduino
-        codes and breadboard electronic circuits`,
-        `Monitored, assessed and acted upon the performance
-        of students by conducting consultations during and after lab
-        sessions`,
-        `Evaluated and graded students’ lab reports`,
+        `Explained concepts, clarified doubts and assisted 10 students with weekly lab assignments`,
+        `Monitored, assessed and acted upon their performance by conducting consultations 
+        during/after lab sessions and guided them in ground-up development of a sensor-assisted autonomous robot`,
+        `Topics Included: DC/AC Principles, Signal Processing, Arduino Programming`,
       ],
     };
 
-    const Astar = {
-      Role: "software engineering intern",
+    const astar = {
+      Role: "embedded software engineering intern",
       Name: "a*star",
       Duration: "may 2019 – aug 2019",
       Description: [
-        `Developed a visualization dashboard for industrial
-          application using HTML, CSS, Javascript and Semantic UI`,
-        `Reverse-engineered the VSE004 Windows sensor
-          application using Python and Wireshark to replicate it and
-          extract sensor data for a ML program running on a
-          Linux-based embedded system`,
-        `Enabled Golang-based EdgeXFoundry in the I2R IOT
-          platform using MongoDB as the database`,
-        `Configured a low-power border router to enable IPv6
-          connections between a SmartMesh IP network and Internet`,
-        `Carried out adhoc power data logging measurements
-          for various IOT devices using a DC power analyser`,
+        `Established an IoT middleware platform that facilitated interoperability between 
+        IoT edge devices and applications and developed visualization dashboards to monitor 
+        real-time data from those devices`,
+        `Solved problems of scraping sensor data from a native Windows application by 
+        reverse-engineering it using Wireshark and rebuilding it on a Linux-based embedded 
+        system using Python and MQTT listeners`,
+        `Tech Stack: Golang, Python, JavaScript, EdgeXFoundry, Wireshark, SmartMesh, MQTT`,
       ],
     };
     return (
@@ -71,16 +113,16 @@ class ExperiencesContainer extends Component {
           <div className="container">
             <h3 className="text-center">My Work Experience</h3>
 
-            <div className="col-md-2 exp-left">
-              {/* <!-- <img src="images/abhishek.jpg" alt="" /> --> */}
-            </div>
+            <div className="col-md-2 exp-left" />
             <div className="col-md-8  exp_right">
               <div className="exp_grids">
-                {/* <Experience CompanyDetails={Hp} />
-                <Experience CompanyDetails={Nus_SA} /> */}
-                <Experience CompanyDetails={Amaris} />
-                <Experience CompanyDetails={Nus_TA} />
-                <Experience CompanyDetails={Astar} />
+                <Experience CompanyDetails={garuda} />
+                <Experience CompanyDetails={orbital_TA} />
+                <Experience CompanyDetails={sensorflow} />
+                <Experience CompanyDetails={hp} />
+                <Experience CompanyDetails={amaris} />
+                <Experience CompanyDetails={cg1111_TA} />
+                <Experience CompanyDetails={astar} />
               </div>
             </div>
             <div className="clearfix"></div>
