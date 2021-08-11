82660af273d9472f9bbe5abbdf26f3ce83a25191
Abhishek <saiabhishek.karnati@u.nus.edu>
2021-08-11 11:03:04 +0800

    minor changes

---
+++ build/precache-manifest.f1d985aafe3a23647f1440ed1c74b013.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1e401c6df51b827e1aeb20ab89750f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5e1c84c8a9873fbac9c7",
+    "url": "/portfolio/static/css/main.f8c12985.chunk.css"
+  },
+  {
+    "revision": "a9967a3fa3d5d8628d54",
+    "url": "/portfolio/static/js/2.e16bb24c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.e16bb24c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e1c84c8a9873fbac9c7",
+    "url": "/portfolio/static/js/main.c4fff47d.chunk.js"
+  },
+  {
+    "revision": "c7cc0f75eead4b3ec541",
+    "url": "/portfolio/static/js/runtime-main.8131c2c7.js"
+  }
+]);
